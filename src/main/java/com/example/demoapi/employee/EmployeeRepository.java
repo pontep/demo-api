b96ad227e81a3e467e6b25973ead65015b3b7063
jamikorn.jongka <jamikorn.jongka@priorsolution.co.th>
2021-01-25 15:35:30 +0700

    fix: required args constructor

--- src/main/java/com/example/demoapi/employee/EmployeeRepository.java
+++ src/main/java/com/example/demoapi/employee/EmployeeRepository.java
@@ -1,6 +1,5 @@
 package com.example.demoapi.employee;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
--- src/main/java/com/example/demoapi/employee/EmployeeService.java
+++ src/main/java/com/example/demoapi/employee/EmployeeService.java
@@ -1,6 +1,5 @@
 package com.example.demoapi.employee;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
