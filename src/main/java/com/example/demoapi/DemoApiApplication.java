53cd1ce54c8554b981c68a27e87bb6b916880e72
Pontep Thaweesup <pontep.taveeshup@gmail.com>
2021-01-28 16:23:07 +0700

    add User for testing redis

--- src/main/java/com/example/demoapi/DemoApiApplication.java
+++ src/main/java/com/example/demoapi/DemoApiApplication.java
@@ -2,12 +2,14 @@ package com.example.demoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class DemoApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApiApplication.class, args);
+    }
 
 }
